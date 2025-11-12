cb477a9d4dcdfd8082b9355143777781c6eb0ad5
VincentVergioUMN <vincent.vergio@student.umn.ac.id>
2025-11-12 10:44:46 +0700

    Final Keyword

---
+++ src/week10/vincent/id/ac/umn/a.java
@@ -0,0 +1,9 @@
+package week10.vincent.id.ac.umn;
+
+final class a {
+
+	final void method1()
+	{
+		System.out.println("This is a final method.");
+	}
+}
---
+++ src/week10/vincent/id/ac/umn/b.java
@@ -0,0 +1,10 @@
+package week10.vincent.id.ac.umn;
+
+public class b extends a {
+
+	void method1()
+	{
+		System.out.println("Illegal");
+	}
+
+}
